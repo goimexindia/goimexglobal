e6f23010c00f546807951b36f716881c4bfc981e
Sunil Jangira <goimexindia@gmail.com>
2021-06-04 17:12:23 +0530

    s3 bucket added

--- blog/models.py
+++ blog/models.py
@@ -88,11 +88,11 @@ class Post(models.Model):
 
     def save(self, *args, **kwargs):
         super().save()
-        image = Image.open(self.image.path)
-        if image.height > 3000 or image.width > 3000:
-            output_size = (300, 300)
-            image.thumbnail(output_size)
-            image.save(self.image.path)
+       # image = Image.open(self.image.path)
+        #if image.height > 300 or image.width > 300:
+         #   output_size = (300, 300)
+          #  image.thumbnail(output_size)
+           # image.save(self.image.path)
 
     def total_likes(self):
         return self.likes.count()

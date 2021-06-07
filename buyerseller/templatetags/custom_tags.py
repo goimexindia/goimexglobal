f8fff8cec2560792a4fc2f5f8857474bc29888ec
Sunil Jangira <goimexindia@gmail.com>
2021-06-07 13:33:36 +0530

    faq

--- buyerseller/templatetags/custom_tags.py
+++ buyerseller/templatetags/custom_tags.py
@@ -1,8 +1,30 @@
 from django import template
 
+from accounts.models import Profile
+from blog.models import Post
+from buyerseller.models import Product
+
 register = template.Library()
 
 
 @register.filter(name='gt')
 def gt(a, b):
     return a > 1
+
+
+@register.inclusion_tag("latest.html")
+def show_latest_name(total=5):
+    last = Product.objects.order_by('-id')[:total]
+    return {'last': last}
+
+
+@register.inclusion_tag("latestpost.html")
+def show_latest_post(total=5):
+    last = Post.objects.order_by('-id')[:total]
+    return {'last': last}
+
+
+@register.inclusion_tag("latestcompany.html")
+def show_latest_company(total=5):
+    last = Profile.objects.exclude(organization__isnull=True).order_by('-id')[:total]
+    return {'last': last}

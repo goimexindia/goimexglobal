9ada0c43a689f6b0db9d07620704e7c73ac699a4
Sunil Jangira <goimexindia@gmail.com>
2021-06-08 09:48:59 +0530

    faq

--- buyerseller/templatetags/custom_tags.py
+++ buyerseller/templatetags/custom_tags.py
@@ -28,3 +28,9 @@ def show_latest_post(total=5):
 def show_latest_company(total=5):
     last = Profile.objects.exclude(organization__isnull=True).order_by('-id')[:total]
     return {'last': last}
+
+
+@register.inclusion_tag("latestproduct.html")
+def show_latest_product(total=8):
+    last = Product.objects.order_by('-id')[:total]
+    return {'last': last}

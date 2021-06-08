b46dcc751d3026e81615f4fffeea5a8f569543ae
Sunil Jangira <goimexindia@gmail.com>
2021-06-08 17:48:40 +0530

    faq

--- buyerseller/views.py
+++ buyerseller/views.py
@@ -807,29 +807,4 @@ class ProductListView(generic.ListView):
         return context
 
 
-class Category(MPTTModel):
-    name = models.CharField(max_length=50, unique=True)
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
-    slug = models.SlugField()
 
-    class MPTTMeta:
-        order_insertion_by = ['name']
-
-    class Meta:
-        unique_together = (('parent', 'slug',))
-        verbose_name_plural = 'categories'
-
-    def get_slug_list(self):
-        try:
-            ancestors = self.get_ancestors(include_self=True)
-        except:
-            ancestors = []
-        else:
-            ancestors = [i.slug for i in ancestors]
-        slugs = []
-        for i in range(len(ancestors)):
-            slugs.append('/'.join(ancestors[:i + 1]))
-        return slugs
-
-    def __str__(self):
-        return self.name

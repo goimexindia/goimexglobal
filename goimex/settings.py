4dff522dade05cd2bf535b4f8802a5a165450073
Sunil Jangira <goimexindia@gmail.com>
2021-08-08 19:49:41 +0530

    google

--- goimex/settings.py
+++ goimex/settings.py
@@ -114,7 +114,7 @@ TEMPLATES = [
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.facebook.FacebookOAuth2',
-    'social_core.backends.twitter.TwitterOAuth',
+   'social_core.backends.twitter.TwitterOAuth',
     'social_core.backends.github.GithubOAuth2',
     'django.contrib.auth.backends.ModelBackend',
     "allauth.account.auth_backends.AuthenticationBackend",

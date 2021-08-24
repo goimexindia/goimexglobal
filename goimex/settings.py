cb97440cde6287f1f2e9eccd5a4b1973e91bf526
Sunil Jangira <goimexindia@gmail.com>
2021-08-25 04:36:41 +0530

    testimoninal

--- goimex/settings.py
+++ goimex/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 from pathlib import Path
 from decouple import config, Csv
 from unipath import Path
-from .secret_settings import *
+from .sec import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
